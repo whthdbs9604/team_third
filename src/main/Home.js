ab0bd1112d4ed8bd4184871d00f9db6905427a62
whthdbs9604 <whthdbs9604@naver.com>
2026-06-02 10:20:49 +0900

    로딩 시간 단축 수정
    Home.js 수정

--- src/main/Home.js
+++ src/main/Home.js
@@ -1,24 +1,39 @@
+import { lazy, Suspense } from "react";
 import MainSlide from "./MainSlide";
 import Enjoy from "./Enjoy";
-import Rank from "./Rank";
-import EventBanner from "./EventBanner";
-import Discount from "./Discount"
-import Genre from "./Genre";
-import ShortVideo from "./ShortVideo"
+
+// 하단 컴포넌트들을 lazy(동적 불러오기)로 변경
+const Rank = lazy(() => import("./Rank"));
+const EventBanner = lazy(() => import("./EventBanner"));
+const Discount = lazy(() => import("./Discount"));
+const Genre = lazy(() => import("./Genre"));
+const ShortVideo = lazy(() => import("./ShortVideo"));
 
 export default function Home() {
-
-    return(
+    return (
         <>
             <MainSlide />
             <Enjoy />
-            <Rank />
-            <EventBanner />
-            <Discount />
-            <Genre />
-            <ShortVideo />
-        </>
-        
-    )
 
+            <Suspense fallback={<div style={{ height: '300px' }} />}>
+                <Rank />
+            </Suspense>
+            
+            <Suspense fallback={<div style={{ height: '150px' }} />}>
+                <EventBanner />
+            </Suspense>
+            
+            <Suspense fallback={<div style={{ height: '300px' }} />}>
+                <Discount />
+            </Suspense>
+            
+            <Suspense fallback={<div style={{ height: '400px' }} />}>
+                <Genre />
+            </Suspense>
+            
+            <Suspense fallback={<div style={{ height: '500px' }} />}>
+                <ShortVideo />
+            </Suspense>
+        </>
+    );
 }
