b0ed8bc75e626e73dcf5e03cc7d2a42f32a8da0b
whthdbs9604 <whthdbs9604@naver.com>
2026-06-02 15:01:25 +0900

    로그인 페이지 로고 테스트

--- src/sub/Login.js
+++ src/sub/Login.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useOutletContext } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Login({title}) {
 
@@ -10,6 +11,8 @@ export default function Login({title}) {
 
     return(
         <div id='login_wrap'>
+
+            <h3><Link to="/"><img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="로고" /></Link></h3>
 
         </div>
         
