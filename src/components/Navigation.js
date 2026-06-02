ea490a64db8274b9e61da59aba30c3f207b1d3c9
whthdbs9604 <whthdbs9604@naver.com>
2026-06-02 16:30:03 +0900

    메뉴 페이지, 로그인 페이지, 마이페이지 완료

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,7 +9,7 @@ export default function Header() {
                     <li><Link to="/"><img src={`${process.env.PUBLIC_URL}/img/location_icon.png`} alt="위치"/><p>위치</p></Link></li>
                     <li><Link to="/"><img src={`${process.env.PUBLIC_URL}/img/home_icon.png`} alt="홈"/><p>홈</p></Link></li>
                     <li><Link to="/"><img src={`${process.env.PUBLIC_URL}/img/search_icon.png`} alt="검색"/><p>검색</p></Link></li>
-                    <li><Link to="/"><img src={`${process.env.PUBLIC_URL}/img/mypage_icon.png`} alt="마이페이지"/><p>마이페이지</p></Link></li>
+                    <li><Link to="/mypage"><img src={`${process.env.PUBLIC_URL}/img/mypage_icon.png`} alt="마이페이지"/><p>마이페이지</p></Link></li>
                 </ul>
             </div>
                 
