cca091092505fbb978113813510d28e94a283246
hayoungipnida <fruitdelapassi00n@gmail.com>
2026-06-04 14:21:19 +0900

    예매완료 페이지 + 달력 약간 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import BottomSheetCalendar from "./BottomSheetCalendar";
 import { SEAT_INFO } from "./seatData";
-
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import BookingComplete from "./BookingComplete";
 
 function App() {
 
   return (
     <div className="App">
-
-
-       <BottomSheetCalendar
-       seatInfo={SEAT_INFO}
-        onConfirm={(date) => {
-          console.log("선택된 날짜:", date);
-          alert(`${date.year}년 ${date.month + 1}월 ${date.date}일 예매 진행!`);
-        }}
-      />
+<BrowserRouter>
+       <Routes>
+        <Route path="/" element={
+          <BottomSheetCalendar
+            seatInfo={SEAT_INFO}
+            onConfirm={(date) => {
+              console.log("선택된 날짜:", date);
+            }}
+          />
+        } />
+        <Route path="/booking/complete" element={<BookingComplete />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
